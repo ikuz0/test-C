ec4a1c5652b3d0cb4b175b7778c6b064070a16fd
IkuzO <Kuznetsovilya469@yandex.ru>
2022-08-31 02:10:28 +0300

    ДЗ_3

---
+++ HQ_003/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+string numberA = Console.ReadLine();
+int numberAint = Convert.ToInt32(numberA);
+
+if(numberAint%2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
