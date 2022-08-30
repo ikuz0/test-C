49275af1039f754ba549c87c4308192233c232b9
IkuzO <Kuznetsovilya469@yandex.ru>
2022-08-31 02:03:50 +0300

    ДЗ2

---
+++ HQ_002/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите первое число: ");
+string numberA = Console.ReadLine();
+int numberAint = Convert.ToInt32(numberA);
+Console.WriteLine("Введите второе число: ");
+string numberB = Console.ReadLine();
+int numberBint = Convert.ToInt32(numberB);
+Console.WriteLine("Введите третье число: ");
+string numberC = Console.ReadLine();
+int numberCint = Convert.ToInt32(numberC);
+if(numberAint>numberBint)
+{
+    if(numberAint>numberCint)
+    {
+    Console.WriteLine(numberAint);
+    }
+    else
+    {
+        Console.WriteLine(numberCint);
+    }
+}
+else
+{
+    if(numberBint>numberCint)
+    {
+        Console.WriteLine(numberBint);
+    }
+    else
+    {
+        Console.WriteLine(numberCint);
+    }
+}
