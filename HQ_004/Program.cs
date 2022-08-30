07eadd7da7f006e1bb2733ca2de5997c88e9a2b3
IkuzO <Kuznetsovilya469@yandex.ru>
2022-08-31 02:23:02 +0300

    ДЗ_4

---
+++ HQ_004/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+string numberA = Console.ReadLine();
+int numberAint = Convert.ToInt32(numberA);
+int i=1;
+while(i<(numberAint+1))
+{
+    if(i%2 < 1)
+    {
+        Console.WriteLine(" ");
+        Console.Write(i);
+    }
+    i++;
+}
