bbe3d811adc40c34b80804a8031b490da5d4476f
IkuzO <Kuznetsovilya469@yandex.ru>
2022-09-01 01:18:40 +0300

    Задание 2_1

---
+++ HQ_201/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int num = 0;
+
+if(1000 > number)
+{
+    if(number > -1000)
+    {
+       num = Math.Abs(number%100/10);
+       Console.WriteLine(number);
+       Console.WriteLine(num);
+    }
+    else
+    {
+        Console.WriteLine("Число не трехзначное");
+    }
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
