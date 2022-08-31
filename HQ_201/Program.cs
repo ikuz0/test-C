53735d42ad3768656d6cb373a0cc9e5720199d10
IkuzO <Kuznetsovilya469@yandex.ru>
2022-09-01 02:12:37 +0300

    Задание 2 случайное число

--- HQ_201/Program.cs
+++ HQ_201/Program.cs
@@ -1,15 +1,16 @@
-﻿Console.WriteLine("Введите трехзначное число: ");
-int number = int.Parse(Console.ReadLine()!);
+﻿int number = new Random().Next(10, 1000);
+Console.WriteLine("Введите трехзначное число: ");
+
 
 int num = 0;
 
-if(1000 > number)
+if (1000 > number)
 {
-    if(number > -1000)
+    if (number > -1000)
     {
-       num = Math.Abs(number%100/10);
-       Console.WriteLine(number);
-       Console.WriteLine(num);
+        num = Math.Abs(number % 100 / 10);
+        Console.WriteLine(number);
+        Console.WriteLine(num);
     }
     else
     {
--- HQ_202/Program.cs
+++ HQ_202/Program.cs
@@ -1,17 +1,18 @@
-﻿Console.WriteLine("Введите число: ");
-int number = int.Parse(Console.ReadLine()!);
+﻿int number = new Random().Next(1, 100000);
+Console.WriteLine("Введите число: ");
+
 //определение колличества цифр в числе
 int len = number.ToString().Length;
 
 int DigNum(int num, int n)
 {
-    return num/(int)Math.Pow(10,len-n)%10;
+    return num / (int)Math.Pow(10, len - n) % 10;
 }
-if(number<100)
+if (number < 100)
 {
     Console.WriteLine("третьей цифры нет");
 }
 else
 {
-    Console.WriteLine($"{number} -> {DigNum(number,3)}");
+    Console.WriteLine($"{number} -> {DigNum(number, 3)}");
 }
