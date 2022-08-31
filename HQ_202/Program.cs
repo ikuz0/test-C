8e2683a556947ef583585d3489081fe1eb5626a9
IkuzO <Kuznetsovilya469@yandex.ru>
2022-09-01 01:53:45 +0300

    Задание 2_2 добавил коментарий

--- HQ_202/Program.cs
+++ HQ_202/Program.cs
@@ -1,6 +1,8 @@
 ﻿Console.WriteLine("Введите трехзначное число: ");
 int number = int.Parse(Console.ReadLine()!);
+//определение колличества цифр в числе
 int len = number.ToString().Length;
+
 int DigNum(int num, int n)
 {
     return num/(int)Math.Pow(10,len-n)%10;
