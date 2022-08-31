6df0202581ab2255ae4bc826d918ffe3f590d2d9
IkuzO <Kuznetsovilya469@yandex.ru>
2022-09-01 01:52:03 +0300

    Задание 2_2

---
+++ HQ_202/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+int len = number.ToString().Length;
+int DigNum(int num, int n)
+{
+    return num/(int)Math.Pow(10,len-n)%10;
+}
+
+Console.WriteLine($"{number} -> {DigNum(number,3)}");
