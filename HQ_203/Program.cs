271fe705b428f73dd10b07988c268e6d92a2ffcf
IkuzO <Kuznetsovilya469@yandex.ru>
2022-09-01 02:25:44 +0300

    Задание 2_3 дни недели

---
+++ HQ_203/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Next(1, 8);
+Console.WriteLine("Введите номер дня недели: ");
+
+
+if (number < 6)
+{
+    Console.WriteLine($"{number} -> Нет");
+}
+else
+{
+    Console.WriteLine($"{number} -> Да");
+}
