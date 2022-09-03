52781bf55c3c0774e295246082b862cc97e7e9b0
IkuzO <Kuznetsovilya469@yandex.ru>
2022-09-03 21:04:28 +0300

    задание 502

---
+++ HQ_502/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задача 36: 
+Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(-100, 100);
+        index++;
+    }
+}
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void СountingEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ((i % 2) != 0)
+        {
+            count = count + array[i];
+            //Console.WriteLine($"подсчет>>{count}");
+        }
+    }
+    Console.WriteLine($"Колличество четных элементов массива >> {count}");
+}
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+СountingEven(array);
