619c4e8e1b95031cb5edfdbedda0a2d65af6c429
IkuzO <Kuznetsovilya469@yandex.ru>
2022-09-03 20:53:07 +0300

    задание 501

---
+++ HQ_501/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(100, 1000);
+        index++;
+    }
+}
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void СountingEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ((array[i] % 2) == 0)
+        {
+            count++;
+            // Console.WriteLine($"Элемент>> {array[i]} подсчет>>{count}");
+        }
+    }
+    Console.WriteLine($"Колличество четных элементов массива >> {count}");
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+СountingEven(array);
