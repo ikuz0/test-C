94b273ea1d38a6030ef436fcd0073455dd2d966c
IkuzO <Kuznetsovilya469@yandex.ru>
2022-09-04 01:00:39 +0300

    задание 601

---
+++ HQ_601/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+
+*/
+
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1}-й элемент: >> ");
+        array[i] = int.Parse(Console.ReadLine()!);
+
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void Counnt(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Колличество элементов больше 0 >>>> {count}");
+}
+
+Console.WriteLine("Введите колличество элементов: ");
+int num = int.Parse(Console.ReadLine()!);
+int[] array = new int[num];
+FillArray(array);
+PrintArray(array);
+Counnt(array);
