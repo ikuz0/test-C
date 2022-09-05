b1ad3cc8a9d832020d8e2e1ba636b6f14914dbfc
IkuzO <Kuznetsovilya469@yandex.ru>
2022-09-05 23:03:32 +0300

    Двумерный массив вещественных чисел 702

---
+++ HQ_702/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задача 50. Напишите программу, 
+которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, 
+что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+17 -> такого числа в массиве нет
+
+*/
+
+int ni, nj;
+Console.WriteLine($"Введите номер строки : ");
+int.TryParse(Console.ReadLine(), out ni);
+Console.WriteLine($"Введите номер столбца: ");
+int.TryParse(Console.ReadLine(), out nj);
+
+Console.WriteLine();
+
+int m = 5, n = 5;
+double[,] array = new double[5, 5];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+        Console.Write($"{array[i, j]}  ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+if (ni < m && nj < n)
+{
+    Console.WriteLine($" значение >> {array[ni, nj]}");
+}
+else
+{
+    Console.WriteLine($" Такого элемента нет ");
+}
