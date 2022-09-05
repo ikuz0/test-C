9849ab8d7d91deb7b4437e20613b12842dc10fac
IkuzO <Kuznetsovilya469@yandex.ru>
2022-09-05 22:42:58 +0300

    Двумерный массив вещественных чисел

---
+++ HQ_701/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+
+0,5     7    -2  -0,2
+  1  -3,3     8  -9,9
+  8   7,8  -7,1     9
+*/
+int m, n;
+Console.WriteLine($"Введите колличество строк: ");
+int.TryParse(Console.ReadLine(), out m);
+Console.WriteLine($"Введите колличество столбцов: ");
+int.TryParse(Console.ReadLine(), out n);
+
+double[,] array = new double[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().NextDouble() * new Random().Next(-10, 10);
+        Console.Write($"{array[i, j]:F1} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
