6ad7f98d49e32c9387fa55f493a9c010b2c0daa2
IkuzO <Kuznetsovilya469@yandex.ru>
2022-09-11 16:40:35 +0300

    лекция

---
+++ EX_900/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+**Задача 63.** Задайте значение N. 
+Напишите программу, которая выведет все натуральные числа 
+в промежутке от 1 до N
+
+Например:
+N = 5 -> "1,2,3,4,5"
+N = 6-> "1,2,3,4,5,6"
+*/
+
+int N;
+Console.Write($" Введите N:  ");
+int.TryParse(Console.ReadLine(), out N);
+
+PrintNUmbers(1, N);
+
+void PrintNUmbers(int start, int end)
+{
+    Console.Write($"{start} ");
+    if (start < end)
+        PrintNUmbers(start + 1, end);
+}
