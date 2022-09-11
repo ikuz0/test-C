57de6827749c47f0b1cccebc630f108a49d709a9
IkuzO <Kuznetsovilya469@yandex.ru>
2022-09-11 14:25:03 +0300

    804

---
+++ HQ_804/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 60.
+Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, 
+добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
+*/
+
+void FillArray(int[,,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[i, j, k] = new Random().Next(1, 10);
+                Console.Write($"{arr[j, k, i]}({j},{k},{i})  ");
+            }
+            Console.WriteLine();
+        }
+    }
+    Console.WriteLine();
+}
+
+int[,,] array = new int[2, 2, 2];
+
+FillArray(array);
